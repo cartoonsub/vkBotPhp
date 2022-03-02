0dac0effd304b6609c9f72f45dd3fb8c8e29b30f
aTaskaev <30393521+cartoonsub@users.noreply.github.com>
2022-03-02 12:03:50 +0300

    vkBot

--- vkBot.php
+++ vkBot.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -7,76 +9,43 @@ ini_set('display_startup_errors', 1);
 require_once 'Src/VkBot.php';
 
 $Parser = new VkBot;
-$results = $Parser->run((bool)$_GET['skip'] ?? '');
+$skip = (bool)$_GET['skip'] ?? false;
+$results = $Parser->run($skip);
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-
-    <style>
-        body {
-            background: rgba(0, 0, 0, 0.8);
-        }
-
-        img {
-            display: block;
-            width: 100%;
-        }
-
-        .main {
-            display: block;
-            margin: auto;
-            width: 80%;
-            background: white;
-            margin-top: 90px;
-        }
-
-        .block {
-            padding: 10px 30px;
-            margin-bottom: 15px;
-            border-bottom: 1px solid linear-gradient(white, black, white);
-        }
-
-        .gallery {
-            display: flex;
-        }
-
-        .picture {
-            margin-top: 15px;
-            margin-left: 10px;
-            width: 60%;
-        }
-
-        a {
-            color: #43b27d;
-        }
-    </style>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
+
 <body>
     <div class="main">
-        <?php foreach ($results as $group => $items): ?>
-            <h2>Новости по группе <?=$group?></h2>
-            <?php foreach ($items as $item): ?>
+        <?php foreach ($results as $group => $items) : ?>
+            <h2>Новости по группе <?= $group ?></h2>
+            <?php foreach ($items as $item) : ?>
                 <div class="block">
-                    <div class="date"><?=$item['date'];?></div>
-                    <p><?=$item['text'];?></p>
-                    <p><a href="<?=$item['wallLink'];?>" target="new">Ссылка на стену</a></p>
+                    <div class="date"><?= $item['date']; ?></div>
+                    <p><?= $item['text']; ?></p>
+                    <p><a href="<?= $item['wallLink']; ?>" target="new">Ссылка на стену</a></p>
                     <div class="gallery">
                         <?php
-                            if (!empty($item['attachments']['photo'])):
-                                foreach ($item['attachments']['photo'] as $image):?>
-                                    <div class="picture">
-                                        <img src="<?=$image;?>" alt="">
-                                    </div>
-                        <?php endforeach; endif;?>
+                        if (!empty($item['attachments']['photo'])) :
+                            foreach ($item['attachments']['photo'] as $image) : ?>
+                                <div class="picture">
+                                    <img src="<?= $image; ?>" alt="">
+                                </div>
+                        <?php endforeach;
+                        endif; ?>
                     </div>
                 </div>
-            <?php endforeach;?>
-        <?php endforeach;?>
+            <?php endforeach; ?>
+        <?php endforeach; ?>
     </div>
     <?php
 
@@ -86,4 +55,5 @@ $results = $Parser->run((bool)$_GET['skip'] ?? '');
 
     ?>
 </body>
+
 </html>
